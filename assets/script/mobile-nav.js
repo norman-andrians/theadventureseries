da862687ce1671f6ac9d75374834679a30e09f91
norman-andrians <norman25.na@gmail.com>
2022-02-09 20:51:04 +0700

    revisi-8

--- assets/script/mobile-nav.js
+++ assets/script/mobile-nav.js
@@ -11,10 +11,24 @@ navBtn.addEventListener('click', function() {
     if (shownav == false) {
         navBg.style.display = "block";
         navBg.style.backgroundColor = "rgba(0, 0, 0, .4)";
+
+        navBtn.style.marginTop = '-2px';
+
+        $(".bar-1").css({'position':'absolute', 'margin':'0', 'transform':'rotateZ(45deg)'})
+        $(".bar-2").css({'position':'absolute', 'margin':'0', 'display':'none'})
+        $(".bar-3").css({'position':'absolute', 'margin':'0', 'transform':'rotateZ(-45deg)'})
+
         anim = setInterval(moveIn, 2);
     }
     else if (shownav == true) {
         navBg.style.backgroundColor = "transparent";
+
+        navBtn.style.marginTop = 0;
+
+        $(".bar-1").css({'position':'inherit', 'margin':'4px 0', 'transform':'rotateZ(0deg)'})
+        $(".bar-2").css({'position':'inherit', 'margin':'4px 0', 'display':'block'})
+        $(".bar-3").css({'position':'inherit', 'margin':'4px 0', 'transform':'rotateZ(0deg)'})
+
         anim = setInterval(moveOut, 2);
     }
 });
